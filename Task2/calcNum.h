872f3679e2ab4db1a8824827bedbc19015540d7b
Andrey Poluektov <147295632+at1et@users.noreply.github.com>
2023-10-30 12:33:13 +0900

    Update and rename calcF.h to calcNum.h

---
+++ Task2/calcNum.h
@@ -0,0 +1,11 @@
+//Poluektov Andrey, VMK-22
+#include <vector> // Добавьте эту строку для работы с векторами
+using namespace std; // Добавьте эту строку для использования пространства имен std
+
+// Процедура для сложения элементов массива и выведения их под модуль
+int calculateAbsSum(const vector<int>& arr);
+
+// Процедура для заполнения массива из файла и получения его длины
+bool numFromFile(const char* filename, vector<int>& arr);
+
+//
