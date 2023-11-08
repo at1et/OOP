b28d0e602b7a76a8c1fbaaf9a155e663776b8760
Andrey Poluektov <147295632+at1et@users.noreply.github.com>
2023-11-08 22:36:29 +0900

    Update main.cpp

--- Task2/main.cpp
+++ Task2/main.cpp
@@ -1,4 +1,4 @@
-//AUTOR: ANDREY POLUEKTOV VMK-22
+//Autor : Andrey Poluektov 
 #include <iostream>
 #include <cassert>
 #include <vector>
@@ -8,52 +8,53 @@ using namespace std;
 
 int main()
 {
-    int n, 		// объявление переменной для длины массива
-		x, 		// переменная для выбора способа ввода элементов
-	absSum = 0; // объявление переменной для работы с суммой и модулем
-    vector<int> num(n); // Создание вектора
+    int n, x, absSum = 0;
+    vector<double> num; // Используем вещественные числа вместо целых
 
     // Переменные для тестировки
-    int test[] = { 1, 5, 4, 3, 2 };
-    int test2[] = { 1, 5, 4 };
-    int test3[] = { 1, 5, 4, 3, 2, 6, 2 };
+    double test[] = { 1.5, 2.5, 3.5, 4.5, 5.5 };
+    double test2[] = { 1.5, 2.5, 3.5 };
+    double test3[] = { 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5 };
 
-    // Проверка
-    assert(calculateAbsSum(vector<int>(test, test + 5)) == 15);
-    assert(calculateAbsSum(vector<int>(test2, test2 + 3)) == 10);
-    assert(calculateAbsSum(vector<int>(test3, test3 + 7)) == 23);
 
-    cout << "Если вы хотите ввести значения из файла - нажмите 1" << endl << "Если вы хотите кол-во чисел и заполнить их случайными значениями - нажмите 0" << endl;
+
+    cout << "Если вы хотите ввести значения из файла - нажмите 1" << endl
+        << "Если вы хотите указать количество чисел и заполнить их случайными значениями - нажмите 0" << endl;
     cin >> x;
-    switch (x) // Выбор между первым и вторым элементом 
+    switch (x)
     {
-    case 0: // Для ввода кол-ва чисел вручную и заполние их рандомными значениями
+    case 0:
     {
         cout << "Введите количество чисел: " << endl;
-    	cin >> n;	
-    	num.resize(n);	// Позволяет изменять размер контейнера. Размер контейнера может увеличиваться или уменьшаться.
+        cin >> n;
+        num.resize(n);
         for (int i = 0; i < n; i++)
         {
-           	num[i] = rand(); // генерация от 1 до INT_MAX
-            cout << "a" << i + 1 << " " << num[i] << endl; // выводим число
+            num[i] = rand() % 200 - 100; // Генерация от -100 до 100
+            cout << "a" << i + 1 << " " << num[i] << endl;
         }
-		
+
         absSum = calculateAbsSum(num);
 
         cout << "" << endl;
         cout << "Сумма всех элементов массива под модулем: " << absSum << endl;
         cout << "" << endl;
-        saveResultsToFile(num, absSum); // Сохраняем результаты в файл 
-        break;
-     }
-    case 1: //Для ввода чисел из файла
-    {
-        const char* filename = "numbers.txt";
-        vector<int> arr;
 
-        if (numFromFile(filename, arr))
+        string fileName;
+        cout << "Введите имя файла для сохранения результатов: ";
+        cin >> fileName;
+        saveResultsToFile(num, absSum, fileName);
+        break;
+    }
+    case 1:
+    {
+        string fileName;
+        cout << "Введите имя файла с числами: ";
+        cin >> fileName;
+
+        vector<double> arr;
+        if (numFromFile(fileName.c_str(), arr))
         {
-            // Выводим содержимое вектора из файла
             cout << "Содержимое вектора из файла:" << endl;
             for (int i = 0; i < arr.size(); i++)
             {
@@ -65,7 +66,8 @@ int main()
             cout << "" << endl;
             cout << "Сумма всех элементов вектора под модулем: " << absSum << endl;
             cout << "" << endl;
-            saveResultsToFile(num, absSum);
+
+            saveResultsToFile(num, absSum, fileName);
         }
         else
         {
@@ -73,11 +75,8 @@ int main()
         }
         break;
     }
-
-
     default:
         cout << "Неправильный выбор" << endl;
     }
 
-    return 0;
 }
