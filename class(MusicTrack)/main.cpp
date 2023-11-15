34eeb1ab236d11373095d899297f6329de36be56
Andrey Poluektov <147295632+at1et@users.noreply.github.com>
2023-11-15 21:04:17 +0900

    Update main.cpp

--- class(MusicTrack)/main.cpp
+++ class(MusicTrack)/main.cpp
@@ -1,29 +1,31 @@
 #include <iostream>  // Подключение библиотеки для ввода/вывода через консоль
 #include <string>    // Подключение библиотеки для работы со строками
 
+using namespace std;  // Использование пространства имен std
+
 // Объявление класса MusicTrack
 class MusicTrack {
 public:
     // Конструктор класса с параметрами
-    MusicTrack(std::string title, std::string artist, double length, std::string album, std::string genre)
+    MusicTrack(string title, string artist, double length, string album, string genre)
         : title_(title), artist_(artist), length_(length), album_(album), genre_(genre) {}
 
     // Метод для вывода информации о треке
     void printTrackInfo() const {
-        std::cout << "Title: " << title_ << std::endl;    // Вывод названия трека
-        std::cout << "Artist: " << artist_ << std::endl;  // Вывод исполнителя
-        std::cout << "Length: " << length_ << " minutes" << std::endl;  // Вывод длительности трека
-        std::cout << "Album: " << album_ << std::endl;    // Вывод названия альбома
-        std::cout << "Genre: " << genre_ << std::endl;    // Вывод жанра трека
+        cout << "Title: " << title_ << endl;    // Вывод названия трека
+        cout << "Artist: " << artist_ << endl;  // Вывод исполнителя
+        cout << "Length: " << length_ << " minutes" << endl;  // Вывод длительности трека
+        cout << "Album: " << album_ << endl;    // Вывод названия альбома
+        cout << "Genre: " << genre_ << endl;    // Вывод жанра трека
     }
 
 private:
     // Приватные поля класса
-    std::string title_;    // Название трека
-    std::string artist_;   // Имя исполнителя
-    double length_;        // Длительность трека
-    std::string album_;    // Название альбома
-    std::string genre_;    // Жанр трека
+    string title_;    // Название трека
+    string artist_;   // Имя исполнителя
+    double length_;    // Длительность трека
+    string album_;    // Название альбома
+    string genre_;    // Жанр трека
 };
 
 // Главная функция программы
