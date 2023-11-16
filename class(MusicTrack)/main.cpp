4a82db4632527cd59e331ae7578059c3df402755
Andrey Poluektov <147295632+at1et@users.noreply.github.com>
2023-11-16 11:47:54 +0900

    Update main.cpp

--- class(MusicTrack)/main.cpp
+++ class(MusicTrack)/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>  // Подключение библиотеки для ввода/вывода через консоль
 #include <string>    // Подключение библиотеки для работы со строками
 
-using namespace std;  // Использование пространства имен std
+using namespace std;
 
 // Объявление класса MusicTrack
 class MusicTrack {
@@ -17,6 +17,8 @@ public:
         cout << "Length: " << length_ << " minutes" << endl;  // Вывод длительности трека
         cout << "Album: " << album_ << endl;    // Вывод названия альбома
         cout << "Genre: " << genre_ << endl;    // Вывод жанра трека
+        cout << "year" << endl;
+        cout << endl;
     }
 
 private:
@@ -32,7 +34,10 @@ private:
 int main() {
     // Создание экземпляра класса MusicTrack с заданными параметрами
     MusicTrack myTrack("Song Title", "Artist Name", 3.5, "Album Name", "Pop");
+    MusicTrack myAnotherTrack("Goodbye m", "tututut", 10, "meds", "Alternative");
 
     // Вызов метода для вывода информации о треке
     myTrack.printTrackInfo();
+    myAnotherTrack.printTrackInfo();
+
 }
