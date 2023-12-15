c8d25777b4b5cacfbb598a55394636fb6e14cb8f
Andrey Poluektov <147295632+at1et@users.noreply.github.com>
2023-12-15 19:24:35 +0900

    Create MusicTrack.h

---
+++ class(MusicTrack)/MusicTrack.h
@@ -0,0 +1,44 @@
+#pragma once  // Директива предотвращает многократное включение заголовочного файла
+#include <iostream>
+#include <string>
+
+using namespace std;  // Используем пространство имен std
+
+class MusicTrack {
+public:
+    // Конструктор класса с параметрами
+    MusicTrack(const string& title, const string& artist, double length, const string& album, const string& genre, int year);
+
+    // Метод для вывода информации о треке
+    void printTrackInfo() const;
+
+    // Геттеры и сеттеры для полей класса
+    string getTitle() const;   // Геттер для названия трека
+    void setTitle(const string& title);  // Сеттер для названия трека
+
+    string getArtist() const;  // Геттер для исполнителя
+    void setArtist(const string& artist);  // Сеттер для исполнителя
+
+    double getLength() const;  // Геттер для длительности трека
+    void setLength(double length);  // Сеттер для длительности трека
+
+    string getAlbum() const;  // Геттер для названия альбома
+    void setAlbum(const string& album);  // Сеттер для названия альбома
+
+    string getGenre() const;  // Геттер для жанра трека
+    void setGenre(const string& genre);  // Сеттер для жанра трека
+
+    int getYear() const;  // Геттер для года
+    void setYear(int year);  // Сеттер для года
+
+    double getLengthInSeconds() const;  // Дополнительный метод, возвращающий продолжительность в секундах
+
+private:
+    // Приватные поля класса
+    string title_;    // Название трека
+    string artist_;   // Имя исполнителя
+    double length_;    // Длительность трека
+    string album_;    // Название альбома
+    string genre_;    // Жанр трека
+    int year_;        // Год
+};
