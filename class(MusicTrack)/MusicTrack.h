cddf5ff79dba86c920d6776fea5e8f1e1e6426db
Andrey Poluektov <147295632+at1et@users.noreply.github.com>
2024-02-05 13:32:18 +0900

    Update MusicTrack.h

--- class(MusicTrack)/MusicTrack.h
+++ class(MusicTrack)/MusicTrack.h
@@ -1,42 +1,50 @@
 #pragma once  // Директива предотвращает многократное включение заголовочного файла
 #include <iostream>
 #include <string>
-#include <cassert>
 
 using namespace std;  // Используем пространство имен std
 
 /* MusicTrack представляет собой абстракцию музыкального трека,
 содержащую информацию о его названии, исполнителе, длительности, альбоме, жанре и годе выпуска*/
-
-
 class MusicTrack {
 public:
     // Конструктор класса с параметрами
     MusicTrack(const string& title, const string& artist, double length, const string& album, const string& genre, int year);
 
     // Метод для вывода информации о треке
-    void printTrackInfo() const;
+    void printTrackInfo() const; // const не меняет полей класса
 
     // Геттеры и сеттеры для полей класса
-    string getTitle() const;   // Геттер для названия трека
-    void setTitle(const string& title);  // Сеттер для названия трека
 
-    string getArtist() const;  // Геттер для исполнителя
-    void setArtist(const string& artist);  // Сеттер для исполнителя
+    // Геттер для названия трека
+    string getTitle() const;   
+    // Сеттер для названия трека
+    void setTitle(const string& title);  
 
-    double getLength() const;  // Геттер для длительности трека
-    void setLength(double length);  // Сеттер для длительности трека
+    // Геттер для исполнителя
+    string getArtist() const;
+    // Сеттер для исполнителя
+    void setArtist(const string& artist);
 
-    string getAlbum() const;  // Геттер для названия альбома
-    void setAlbum(const string& album);  // Сеттер для названия альбома
+    // Геттер для длительности трека
+    double getLength() const;  
+    // Сеттер для длительности трека
+    void setLength(double length);  
 
-    string getGenre() const;  // Геттер для жанра трека
-    void setGenre(const string& genre);  // Сеттер для жанра трека
+    // Геттер для названия альбома
+    string getAlbum() const; 
+    // Сеттер для названия альбома
+    void setAlbum(const string& album);  
 
-    int getYear() const;  // Геттер для года
-    void setYear(int year);  // Сеттер для года
+    // Геттер для жанра трека
+    string getGenre() const;
+    // Сеттер для жанра трека
+    void setGenre(const string& genre);  
 
-    double getLengthInSeconds() const;  // Дополнительный метод, возвращающий продолжительность в секундах
+    // Геттер для года
+    int getYear() const;
+    // Сеттер для года
+    void setYear(int year);  
 
 private:
     // Приватные поля класса
@@ -47,7 +55,3 @@ private:
     string genre_;    // Жанр трека
     int year_;        // Год
 };
-void test() {
-    MusicTrack myTrack("Song Title", "Artist Name", 3.5, "Album Name", "Pop", 2023);
-    assert(title_ == "Song Title");
-}
