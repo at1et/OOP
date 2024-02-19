c5eaa1437539f3ebbfb246dbea8185149a43ee7d
Andrey Poluektov <147295632+at1et@users.noreply.github.com>
2024-02-19 12:58:07 +0900

    Update main.cpp

--- class(MusicTrack)/main.cpp
+++ class(MusicTrack)/main.cpp
@@ -1,31 +1,24 @@
 #include <iostream>
+#include <cassert>
 #include "MusicTrack.h"
 
 int main() {
-    // Создание экземпляра класса MusicTrack с заданными параметрами
-    MusicTrack myTrack("Song Title", "Artist Name", 3.5, "Album Name", "Pop", 2023);
+	{
+		MusicTrack test("","",0,"","",0);
+		test.setAlbum("Album");
+		test.setArtist("Artist");
+		test.setGenre("Genre");
+		test.setLength(122);
+		test.setTitle("Title");
+		test.setYear(2012);
+		assert(test.getAlbum() == "Album");
+		assert(test.getArtist() == "Artist");
+		assert(test.getGenre() == "Genre");
+		assert(test.getLength() == 122);
+		assert(test.getTitle() == "Title");
+		assert(test.getYear() == 2012);
+			 
 
-    // Используем геттеры для получения информации о треке и выводим ее
-    cout << "Title: " << myTrack.getTitle() << endl; // Получаем и выводим название трека
-    cout << "Artist: " << myTrack.getArtist() << endl; // Получаем и выводим имя исполнителя
-    cout << "Length: " << myTrack.getLength() << " minutes" << endl; // Получаем и выводим длительность трека
-    cout << "Album: " << myTrack.getAlbum() << endl; // Получаем и выводим название альбома
-    cout << "Genre: " << myTrack.getGenre() << endl; // Получаем и выводим жанр трека
-    cout << "Year: " << myTrack.getYear() << endl; // Получаем и выводим год выпуска трека
-    
-    cout << "\n";
 
-
-    // Используем сеттеры для изменения информации о треке
-    myTrack.setTitle("Accordion"); // Устанавливаем новое название трека
-    myTrack.setArtist("MF DOOM"); // Устанавливаем нового исполнителя
-    myTrack.setLength(1.58); // Устанавливаем новую длительность трека
-    myTrack.setAlbum("Madvillain"); // Устанавливаем новое название альбома
-    myTrack.setGenre("Hip-hop"); // Устанавливаем новый жанр трека
-    myTrack.setYear(2004); // Устанавливаем новый год выпуска трека
-
-    // Выводим обновленную информацию о треке
-    myTrack.printTrackInfo();
-
-    return 0;
+	}
 }
