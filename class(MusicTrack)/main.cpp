8e05bda1e916f7207d8999c2340e626198a961a1
Andrey Poluektov <147295632+at1et@users.noreply.github.com>
2024-04-09 22:26:19 +0900

    Update main.cpp

--- class(MusicTrack)/main.cpp
+++ class(MusicTrack)/main.cpp
@@ -2,24 +2,44 @@
 
 int main() {
 	{
+		setlocale(LC_ALL, "RUS");
+
 		MusicTrack test("", "", 0, "", "", 0);
-		test.setAlbum("Album");
-		test.setArtist("Artist");
-		test.setGenre("Genre");
-		test.setLength(122);
-		test.setTitle("Title");
-		test.setYear(2012);
+		test.setAlbum("Звезда");
+		test.setArtist("Дельфин");
+		test.setGenre("Альтернатива");
+		test.setLength(247);
+		test.setTitle("Имя");
+		test.setYear(2004);
 
 		// Проверка
-		assert(test.getAlbum() == "Album");
-		assert(test.getArtist() == "Artist");
-		assert(test.getGenre() == "Genre");
-		assert(test.getLength() == 122);
-		assert(test.getTitle() == "Title");
-		assert(test.getYear() == 2012);
+		assert(test.getAlbum() == "Звезда");
+		assert(test.getArtist() == "Дельфин");
+		assert(test.getGenre() == "Альтернатива");
+		assert(test.getLength() == 247);
+		assert(test.getTitle() == "Имя");
+		assert(test.getYear() == 2004);
 
 		// Вывод класса на экран 
 		test.printTrackInfo();
+		cout << "\n";
+
+		// Создаем объект класса MusicTrack с данными о треке
+		MusicTrack iDontLove("I Don't Love", "Have a nice life", 366, "Deathconsciousness", " Shoegaze, Post-punk, Post-rock, Gothic rock", 2008);
+
+		string trackData = concatenateTrackData(iDontLove);
+
+		// Сохраняем объект в файл
+		saveToFile(iDontLove, "tracks.txt");
+
+		// Читаем данные о треках из файла
+		vector<MusicTrack> tracks = readFromFile("tracks.txt");
+
+		// Выводим информацию о прочитанных треках
+		for (const auto& track : tracks) {
+			track.printTrackInfo();
+			cout << "\n";
+		}
 
 
 
