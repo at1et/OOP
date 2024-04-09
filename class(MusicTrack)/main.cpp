c825e2841ceb42544caf5ef16b49c54679ea924b
Andrey Poluektov <147295632+at1et@users.noreply.github.com>
2024-04-09 12:53:55 +0900

    Update main.cpp

--- class(MusicTrack)/main.cpp
+++ class(MusicTrack)/main.cpp
@@ -1,23 +1,26 @@
-#include <iostream>
-#include <cassert>
 #include "MusicTrack.h"
 
 int main() {
 	{
-		MusicTrack test("","",0,"","",0);
+		MusicTrack test("", "", 0, "", "", 0);
 		test.setAlbum("Album");
 		test.setArtist("Artist");
 		test.setGenre("Genre");
 		test.setLength(122);
 		test.setTitle("Title");
 		test.setYear(2012);
+
+		// Проверка
 		assert(test.getAlbum() == "Album");
 		assert(test.getArtist() == "Artist");
 		assert(test.getGenre() == "Genre");
 		assert(test.getLength() == 122);
 		assert(test.getTitle() == "Title");
 		assert(test.getYear() == 2012);
-			 
+
+		// Вывод класса на экран 
+		test.printTrackInfo();
+
 
 
 	}
