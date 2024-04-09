4c00d0b2afd76941196e50d79ea7ce3866fb96f0
Andrey Poluektov <147295632+at1et@users.noreply.github.com>
2024-04-09 12:53:32 +0900

    Update MusicTrack.h

--- class(MusicTrack)/MusicTrack.h
+++ class(MusicTrack)/MusicTrack.h
@@ -1,6 +1,9 @@
 #pragma once  // Директива предотвращает многократное включение заголовочного файла
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <cassert>
+#include <fstream>
 
 using namespace std;  // Используем пространство имен std
 
@@ -17,9 +20,9 @@ public:
     // Геттеры и сеттеры для полей класса
 
     // Геттер для названия трека
-    string getTitle() const;   
+    string getTitle() const;
     // Сеттер для названия трека
-    void setTitle(const string& title);  
+    void setTitle(const string& title);
 
     // Геттер для исполнителя
     string getArtist() const;
@@ -27,24 +30,24 @@ public:
     void setArtist(const string& artist);
 
     // Геттер для длительности трека
-    double getLength() const;  
+    double getLength() const;
     // Сеттер для длительности трека
-    void setLength(double length);  
+    void setLength(double length);
 
     // Геттер для названия альбома
-    string getAlbum() const; 
+    string getAlbum() const;
     // Сеттер для названия альбома
-    void setAlbum(const string& album);  
+    void setAlbum(const string& album);
 
     // Геттер для жанра трека
     string getGenre() const;
     // Сеттер для жанра трека
-    void setGenre(const string& genre);  
+    void setGenre(const string& genre);
 
     // Геттер для года
     int getYear() const;
     // Сеттер для года
-    void setYear(int year);  
+    void setYear(int year);
 
 private:
     // Приватные поля класса
